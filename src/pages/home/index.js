b77d71eb17882a2542fc914e39934f0d4a82f598
Oleksii Ignatiev <o.ignatiev@knubisoft.com>
2020-05-15 09:10:54 +0300

    moved home component from components/ to bus/

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Home as HomeComponent } from '../../components/home'
+import { Home as HomeComponent } from '../../bus/home'
 
 export const Home = () => (
     <>
