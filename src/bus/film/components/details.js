8d7e00b082dcc4123e739f340bdaab1d1bcd8796
Maryna Bohovyk <shadowy.still@gmail.com>
2020-05-19 16:47:03 +0300

    #4 change genres info

--- src/bus/film/components/details.js
+++ src/bus/film/components/details.js
@@ -1,9 +1,12 @@
 // Core
 import React from "react";
-import {Avatar, Descriptions} from "antd";
+import {Avatar, Descriptions, Tag} from "antd";
 
 
 export const Details = ({filmDetails}) => {
+    const genres = filmDetails.genres && filmDetails.genres.map(genre=>
+        <Tag color="cyan" key={genre}>{genre}</Tag>
+    );
     return (
         <Descriptions bordered column={3}>
             <Descriptions.Item label="Title">{filmDetails.title}</Descriptions.Item>
@@ -14,7 +17,7 @@ export const Details = ({filmDetails}) => {
             </Descriptions.Item>
             <Descriptions.Item label="Overview" span={2}>{filmDetails.overview}</Descriptions.Item>
             <Descriptions.Item label="Genres">
-                {filmDetails.genres}
+                {genres}
             </Descriptions.Item>
             <Descriptions.Item label="Vote count">{filmDetails.vote_count}</Descriptions.Item>
             <Descriptions.Item label="Vote average">{filmDetails.vote_average}</Descriptions.Item>
